4edacedecb6e7808ff7d5d0c277338df46ff1318
hpaulgs <henrypaulgar.2023.alumn@cescristorey.com>
2025-01-25 11:03:17 +0100

    Filtro agua

---
+++ quietsea/scripts/escenario_principal.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+var velocidad_escenario = 50
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	$Camera2D.position.y -= velocidad_escenario * delta
+	$RigidBody2D/CollisionShape2D3.position.y -= velocidad_escenario * delta
+	#$Camera2D.position.y = position.y
