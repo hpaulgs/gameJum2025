08b91affef0de01c651b39ad448a52f0558d24db
pabloguardiacastro <pablogua.2023.alumn@cescristorey.com>
2025-01-25 09:30:28 +0100

    Tiburon

---
+++ quietsea/scripts/tiburon.gd
@@ -0,0 +1,12 @@
+extends CharacterBody2D
+
+
+var SPEED = -100.0
+
+func _ready() -> void:
+	add_to_group("peces")
+	
+
+func _physics_process(delta: float) -> void:
+	velocity.x = SPEED
+	move_and_slide()
