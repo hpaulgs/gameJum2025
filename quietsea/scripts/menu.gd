37f3e482092ba01f4732d1f397061c4245ce9efd
pabloguardiacastro <pablogua.2023.alumn@cescristorey.com>
2025-01-25 11:48:38 +0100

    Menú de inicio

---
+++ quietsea/scripts/menu.gd
@@ -0,0 +1,36 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$Atras.visible = false
+	$TextoControles.visible = false
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_jugar_pressed() -> void:
+	Controlador.goto_scene("res://escenas/escenarioPrincipal.tscn")
+
+
+func _on_controles_pressed() -> void:
+	$Atras.visible = true
+	$TextoControles.visible = true
+	$Jugar.visible = false
+	$Controles.visible = false
+	$Salir.visible = false
+
+
+func _on_salir_pressed() -> void:
+	get_tree().quit()
+
+
+func _on_atras_pressed() -> void:
+	$Atras.visible = false
+	$TextoControles.visible = false
+	$Jugar.visible = true
+	$Controles.visible = true
+	$Salir.visible = true
