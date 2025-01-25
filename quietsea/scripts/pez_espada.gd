5ccaa1961bd4e1b6540a0d10f357b3d51d19f9e6
NataliaSerantesCortez <nataliaayelenser.2023.alumn@cescristorey.com>
2025-01-25 17:10:57 +0100

    distancia

--- quietsea/scripts/pez_espada.gd
+++ quietsea/scripts/pez_espada.gd
@@ -1,17 +1,27 @@
 extends CharacterBody2D
 
 const danio = -30
-
 var SPEED = -200.0
+var submarino: CharacterBody2D
+var distancia_activacion = 600.0
+var activado = false
 
 func _ready() -> void:
+	submarino = get_node("/root/escenarioPrincipal/Submarino")
 	add_to_group("peces")
 	
 
 func _physics_process(delta: float) -> void:
-	velocity.x = SPEED
-	move_and_slide()
-
+	var distancia = global_position.distance_to(submarino.global_position)
+	if distancia <= distancia_activacion:
+		activado = true
+		
+	if activado:
+		velocity.x = SPEED
+		move_and_slide()
+	else:
+		velocity.x = 0	
+		
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("jugador"):
--- quietsea/scripts/tiburon.gd
+++ quietsea/scripts/tiburon.gd
@@ -3,16 +3,27 @@ extends CharacterBody2D
 const danio = -30
 
 var SPEED = -100.0
+var submarino: CharacterBody2D
+var distancia_activacion = 700.0
+var activado = false
 
 func _ready() -> void:
 	$Sprite2D/AnimationPlayer.play("movimiento")
+	submarino = get_node("/root/escenarioPrincipal/Submarino")
 	add_to_group("peces")
 	
 
 func _physics_process(delta: float) -> void:
-	velocity.x = SPEED
-	move_and_slide()
-
+	var distancia = global_position.distance_to(submarino.global_position)
+	if distancia <= distancia_activacion:
+		activado = true
+		
+	if activado:
+		velocity.x = SPEED
+		move_and_slide()
+	else:
+		velocity.x = 0
+		
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("jugador"):
