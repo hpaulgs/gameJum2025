eb2960ac016ab9cc03350b0aeb93c0c6c8bd5d84
pabloguardiacastro <pablogua.2023.alumn@cescristorey.com>
2025-01-25 08:59:03 +0100

    Submarino

---
+++ quietsea/scripts/submarino.gd
@@ -0,0 +1,30 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+
+
+func _process(delta: float) -> void:
+
+	if Input.is_action_just_pressed("ui_up"):
+		velocity.y = SPEED
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var directionx := Input.get_axis("ui_left", "ui_right")
+	if directionx:
+		velocity.x = directionx * SPEED
+		if velocity.x < 0:
+			$Sprite2D.flip_h = false
+		else:
+			$Sprite2D.flip_h = true
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		
+	var directiony := Input.get_axis("ui_up", "ui_down")
+	if directiony:
+		velocity.y = directiony * SPEED
+	else:
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+
+	move_and_slide()
